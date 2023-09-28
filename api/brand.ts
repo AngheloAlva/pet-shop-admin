5191397c69037d3aefcdf956f8b927f647cd0320
Anghelo Alva <ance.anghelo@gmail.com>
2023-09-28 12:51:19 -0300

    install axios and create petition functions for Brands

---
+++ api/brand.ts
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+import type { Brand } from '@/types'
+
+const createBrand = async (brand: Brand): Promise<void> => {
+  try {
+    const response = await axios.post('http://localhost:3001/brands', brand)
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const getBrands = async (): Promise<Brand[]> => {
+  try {
+    const response = await axios.get('http://localhost:3001/brands')
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+const deleteBrand = async (id: string): Promise<void> => {
+  try {
+    const response = await axios.delete(`http://localhost:3001/brands/${id}`)
+    console.log(response.data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export {
+  createBrand,
+  getBrands,
+  deleteBrand
+}
