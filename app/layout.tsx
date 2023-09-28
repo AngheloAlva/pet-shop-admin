08932b33c0cb1e95c77e349bd3619e1d4e336be5
Anghelo Alva <ance.anghelo@gmail.com>
2023-09-28 11:34:02 -0300

    install shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout ({
 }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        {children}
+      </body>
     </html>
   )
 }
