315088bfc0cc0bafc069e2028cb6749093cdbe08
Anghelo Alva <ance.anghelo@gmail.com>
2023-10-01 14:20:44 -0300

    add domain for images, and fix props problem from products

--- components/Mini-product.tsx
+++ components/Mini-product.tsx
@@ -10,13 +10,15 @@ interface MiniProductProps {
 const MiniProduct = ({ product }: MiniProductProps): JSX.Element => {
   return (
     <div className='border rounded-md flex p-2'>
-      <Image src={product.image} alt={product.name} width={70} height={70} />
+      <div>
+        <Image src={product.image[0]} alt={product.name} width={150} height={0} className='rounded-md' />
+      </div>
       <div className='flex flex-col gap-1 max-w-[20rem]'>
         {product.name}
         <p className='ml-5'>Opciones:</p>
-        {product.weightOptions.map((option, index) => (
+        {product.options.map((option, index) => (
           <div key={index} className='ml-5 flex border rounded-md h-8 items-center gap-1 px-2 max-w-[10rem] justify-center'>
-            {option.weight}
+            {option.option}
             <p>-</p>
             {option.price}
           </div>
@@ -33,15 +35,13 @@ const MiniProduct = ({ product }: MiniProductProps): JSX.Element => {
         </p>
         <p className="mx-3">
           Pet Type:
-          {/* {product.petType.map((pet, index) => (
+          {product.petType.map((pet, index) => (
             <span key={index}>{pet}</span>
-          ))} */}
+          ))}
         </p>
         <p className="mx-3">
           Life Stage:
-          {product.lifeStage.map((lifeStage, index) => (
-            <span key={index}>{lifeStage}</span>
-          ))}
+          {product.lifeStage}
         </p>
       </div>
     </div>
